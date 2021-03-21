329696a9e58e324318c55037064a0f479e02cb1c
nikkkhil067 <emnikkhil@gmail.com>
2021-03-21 13:57:09 +0530

    Folders Creation for DataSet

---
+++ FoldersCreation.py
@@ -0,0 +1,33 @@
+# Importing the Libraries Required
+
+import os
+import string
+
+# Creating the directory Structure
+
+if not os.path.exists("dataSet"):
+    os.makedirs("dataSet")
+
+if not os.path.exists("dataSet/trainingData"):
+    os.makedirs("dataSet/trainingData")
+
+if not os.path.exists("dataSet/testingData"):
+    os.makedirs("dataSet/testingData")
+
+# Making folders from 0 to 10 in the training and testing data folders respectively
+for i in range(11):
+    if not os.path.exists("dataSet/trainingData/" + str(i)):
+        os.makedirs("dataSet/trainingData/" + str(i))
+
+    if not os.path.exists("dataSet/testingData/" + str(i)):
+        os.makedirs("dataSet/testingData/" + str(i))
+
+# Making Folders from A to Z in the training and testing data folders respectively
+
+for i in string.ascii_uppercase:
+    if not os.path.exists("dataSet/trainingData/" + i):
+        os.makedirs("dataSet/trainingData/" + i)
+    
+    if not os.path.exists("dataSet/testingData/" + i):
+        os.makedirs("dataSet/testingData/" + i)
+
